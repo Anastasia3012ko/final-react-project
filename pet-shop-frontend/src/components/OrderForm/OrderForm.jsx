a54309bd67a00421522d7c4a5a0e0e3d6e417a03
AnastasiK30 <anastasia301284@gmail.com>
2025-07-10 18:32:37 +0200

    created form for order

--- pet-shop-frontend/src/components/OrderForm/OrderForm.jsx
+++ pet-shop-frontend/src/components/OrderForm/OrderForm.jsx
@@ -2,24 +2,23 @@ import React, { useState } from 'react'
 import styles from './OrderForm.module.css'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
-import MyLargeButton from '../../ui/MyLargeButton/MyLargeButton'
 import { clearCart } from '../../redux/slices/CartSlice'
+import MyButton from '../../ui/MyButton/MyButton'
 
 const OrderForm = ({ length, totalPrice }) => {
-  const products = useSelector(state => state.cart.items)
-    const productsByOrder = products.map(product => ({
-  id: product.id,
-  quantity: product.quantity
-}))
+  const products = useSelector((state) => state.cart.items)
+  const productsByOrder = products.map((product) => ({
+    id: product.id,
+    quantity: product.quantity,
+  }))
 
   const dispatch = useDispatch()
   const [formData, setFormData] = useState({
     name: '',
     phone: '',
-    email: ''
+    email: '',
   })
   const [buttonText, setButtonText] = useState('Order')
-  
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
@@ -29,8 +28,8 @@ const OrderForm = ({ length, totalPrice }) => {
     e.preventDefault()
 
     const orderData = {
-        ...formData,
-        products: productsByOrder
+      ...formData,
+      products: productsByOrder,
     }
 
     try {
@@ -38,20 +37,15 @@ const OrderForm = ({ length, totalPrice }) => {
         'http://localhost:3333/order/send',
         orderData
       )
-    
 
       if (response.data) {
         setButtonText('The Order is Placed')
         alert('Congratulations! ')
 
-
         dispatch(clearCart())
         setFormData({ name: '', phone: '', email: '' })
-        
       } else {
-        alert(
-          'Something went wrong. Please try again!'
-        )
+        alert('Something went wrong. Please try again!')
         setFormData({ name: '', phone: '', email: '' })
         setButtonText('Order')
       }
@@ -64,41 +58,44 @@ const OrderForm = ({ length, totalPrice }) => {
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.headerForm}>
-        <h4>Order details</h4>
-        <p className={styles.orderText}>{length} items</p>
+        <h4 className={styles.header}>Order details</h4>
+        <h4 className={styles.orderText}>{length} items</h4>
         <div className={styles.totalPrice}>
-          <p className={styles.orderText}>Total</p>
+          <h4 className={styles.orderText}>Total</h4>
           <h3>$ {totalPrice}</h3>
         </div>
       </div>
-      <input
-        className={styles.input}
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        placeholder="Name"
-        required
-      />
-      <input
-        className={styles.input}
-        type="tel"
-        name="phone"
-        value={formData.phone}
-        onChange={handleChange}
-        placeholder="Phone number"
-        required
-      />
-      <input
-        className={styles.input}
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        placeholder="Email"
-        required
-      />
-      <MyLargeButton type="submit">{buttonText}</MyLargeButton>
+      <div className={styles.inputField}>
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Name"
+          required
+        />
+        <input
+          className={styles.input}
+          type="tel"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          placeholder="Phone number"
+          required
+        />
+        <input
+          className={styles.input}
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Email"
+          required
+        />
+      </div>
+
+      <MyButton type="submit">{buttonText}</MyButton>
     </form>
   )
 }
